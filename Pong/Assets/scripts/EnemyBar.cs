fe3461321b5444dea63a68e045ba30d5413660d5
Victor Pinheiro <vpinheiro.aguiar@gmail.com>
2017-11-19 17:53:40 -0300

    Bar's movement and divisory line created

---
+++ Pong/Assets/scripts/EnemyBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBar : MonoBehaviour {
+
+	private BoxCollider2D enBarBc;
+	private Rigidbody2D enBarRb;
+	private Rigidbody2D _ballRb;
+	private int enBarDirection = 0;
+
+	public uint enBarVelocity;
+
+	void Start () {
+		enBarRb = GetComponent<Rigidbody2D> ();
+		enBarBc = GetComponent<BoxCollider2D> ();
+
+		_ballRb = GameObject.Find("ball").GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		enBarDirection = (_ballRb.position.y < enBarRb.position.y) ? -1 : 1;
+
+		if (_ballRb.position.y > enBarRb.position.y + enBarBc.size.y/2 
+			|| _ballRb.position.y < enBarRb.position.y - enBarBc.size.y/2)
+			enBarRb.velocity = new Vector2(enBarRb.velocity.x, enBarDirection * ((int) enBarVelocity));
+		else
+			enBarRb.velocity = new Vector2(enBarRb.velocity.x, 0);
+	}
+}
---
+++ Pong/Assets/scripts/PlayerBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBar : MonoBehaviour {
+
+	private Rigidbody2D barRb;
+	private bool upKeyPressed = false, downKeyPressed = false;
+	private int barDirection = 0;
+
+	public uint barVelocity;
+
+	void Start () {
+		barRb = GetComponent<Rigidbody2D> ();	
+		barRb.velocity = new Vector2(barRb.velocity.x, barDirection * ((int) barVelocity));
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			upKeyPressed = true;
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			downKeyPressed = true;
+		}
+
+		if (Input.GetKeyUp (KeyCode.UpArrow)) {
+			upKeyPressed = false;
+		} else if (Input.GetKeyUp (KeyCode.DownArrow)) {
+			downKeyPressed = false;
+		}
+
+		if (upKeyPressed)
+			barDirection = 1;
+		else if (downKeyPressed)
+			barDirection = -1;
+		else
+			barDirection = 0;
+
+		barRb.velocity = new Vector2(barRb.velocity.x, barDirection * ((int) barVelocity));
+	}
+}
