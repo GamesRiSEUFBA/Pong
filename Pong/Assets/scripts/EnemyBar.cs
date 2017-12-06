2549d507d8346a562c5072b9d05758c567afff39
Caiodzj94116 <ene46148@kopqi.com>
2017-12-06 15:54:52 -0300

    Add implementation of PvP gameplay - TODO: rethink which buttons to use for each player and remove the PvP temp blank scene

--- Pong/Assets/scripts/EnemyBar.cs
+++ Pong/Assets/scripts/EnemyBar.cs
@@ -8,7 +8,7 @@ public class EnemyBar : MonoBehaviour {
 	private Rigidbody2D enBarRb;
 	private Rigidbody2D _ballRb;
 	private int enBarDirection = 0;
-
+	private bool upKeyPressed = false, downKeyPressed = false;
 	public int scr2;
 
 	public uint enBarVelocity;
@@ -22,14 +22,40 @@ public class EnemyBar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		_ballRb = GameObject.FindGameObjectWithTag("ball").GetComponent<Rigidbody2D> ();
 
-		enBarDirection = (_ballRb.position.y < enBarRb.position.y) ? -1 : 1;
+		if (GameInit.vsCPU == 1) {
+			_ballRb = GameObject.FindGameObjectWithTag ("ball").GetComponent<Rigidbody2D> ();
 
-		if (_ballRb.position.y > enBarRb.position.y + enBarBc.size.y/2 
-			|| _ballRb.position.y < enBarRb.position.y - enBarBc.size.y/2)
+			enBarDirection = (_ballRb.position.y < enBarRb.position.y) ? -1 : 1;
+
+			if (_ballRb.position.y > enBarRb.position.y + enBarBc.size.y / 2
+			    || _ballRb.position.y < enBarRb.position.y - enBarBc.size.y / 2)
+				enBarRb.velocity = new Vector2 (enBarRb.velocity.x, enBarDirection * ((int)enBarVelocity));
+			else
+				enBarRb.velocity = new Vector2 (enBarRb.velocity.x, 0);
+		} else {
+
+			if (Input.GetKeyDown (KeyCode.Q)) {
+				upKeyPressed = true;
+			} else if (Input.GetKeyDown (KeyCode.A)) {
+				downKeyPressed = true;
+			}
+
+			if (Input.GetKeyUp (KeyCode.Q)) {
+				upKeyPressed = false;
+			} else if (Input.GetKeyUp (KeyCode.A)) {
+				downKeyPressed = false;
+			}
+
+			if (upKeyPressed)
+				enBarDirection = 1;
+			else if (downKeyPressed)
+				enBarDirection = -1;
+			else
+				enBarDirection = 0;
+
 			enBarRb.velocity = new Vector2(enBarRb.velocity.x, enBarDirection * ((int) enBarVelocity));
-		else
-			enBarRb.velocity = new Vector2(enBarRb.velocity.x, 0);
+
+		}
 	}
 }
--- Pong/Assets/scripts/LoadOnClick.cs
+++ Pong/Assets/scripts/LoadOnClick.cs
@@ -7,6 +7,12 @@ public class LoadOnClick : MonoBehaviour {
 
 	public void loadScene(int dest)
 	{
+		if (dest == 2) {
+			GameInit.vsCPU = 0;
+			dest = dest - 1;
+		} else {
+			GameInit.vsCPU = 1;
+		}
 		print (dest);
 		//Application.LoadLevel (opc);
 		SceneManager.LoadScene(dest, LoadSceneMode.Single);
